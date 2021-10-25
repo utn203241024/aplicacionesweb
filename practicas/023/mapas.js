359695263db5d3ba8e83eab8528612ebff783342
Garcia Arguello Abigail <203241024@alumnos.utn.edu.mx>
2021-10-25 12:54:15 -0500

    Practica 024

--- practicas/023/mapas.js
+++ practicas/023/mapas.js
@@ -1,18 +1,19 @@
-let map = new ol.map
+let map = new ol.Map
 (
     {
-        target:'divMapa',
+        target: 'divMapa',
         layers:
-            [
+        [
             new ol.layer.Tile
             ({
                 source: new ol.source.OSM()
             })
-            ],
-        view: new ol.View
-            ({
+        ],
+    view: new ol.View
+        ({
             center: ol.proj.fromLonLat([-98.9865747, 19.4040675]),
             zoom: 17
-            })
+        })
     }
+
 );
