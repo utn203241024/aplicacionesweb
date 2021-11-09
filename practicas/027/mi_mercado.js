c2ab51d4ad5893f310a89452b6c588921059b66d
Garcia Arguello Abigail <203241024@alumnos.utn.edu.mx>
2021-11-09 11:46:07 -0600

    Practica 027

---
+++ practicas/027/mi_mercado.js
@@ -0,0 +1,26 @@
+// Esta llave es unica y la creas dentro de Googl Developers
+const API_KEY_MAPAS = "AIzaSyCp3PLQR6nkkMAVhFR9D9cUPFKt6XBKgYc"
+let map;
+let utnLab;
+let marcador;
+
+// Creacion del script y llamado:
+var script = document.createElement('script');
+script.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY_MAPAS}&callback=initMap`;
+script.async = true;
+
+// Adjuntar la llamada al mapa para su uso 
+window.initMap = function (){
+    utnLab = { lat: 19.38508610306325, lng: -98.99071237696445 };
+    map = new google.maps.Map(document.getElementById("divMapaGoogle"), {
+        center: utnLab,
+        zoom: 19.54,
+    });
+    marcador = new google.maps.Marker({
+        position: utnLab,
+        map: map,
+    });
+};
+
+// Agregar el script a la cabecera 
+document.head.appendChild(script);
